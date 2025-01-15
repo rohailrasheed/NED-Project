feb185eef2f2eafb2b671cd68484c9a0057a1576
Rohail Rasheed <rohailr3@gmail.com>
2025-01-16 02:55:48 +0500

    second commit

---
+++ node_modules/ag-charts-enterprise/dist/types/src/series/gauge-util/properties.d.ts
@@ -0,0 +1,4 @@
+import { _ModuleSupport } from 'ag-charts-community';
+export declare const FILL_MODE: _ModuleSupport.ValidatePredicate;
+export declare const TARGET_MARKER_SHAPE: _ModuleSupport.ValidatePredicate;
+export declare const CORNER_MODE: _ModuleSupport.ValidatePredicate;
